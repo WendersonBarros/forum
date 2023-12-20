21637f27f47a7181b0cbf0f9a78b1d838d636209
WendersonBarros <wendercesar2@gmail.com>
2023-12-19 23:59:25 -0400

    chore: make database more robust to error handling | name the docker container

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,15 +2,30 @@ import database from "infra/database.js"
 
 const status = async (request, response) => {
   const updatedAt = new Date().toISOString();
-  const postgresVersion = await database.query("SELECT version()")
-  const postgresMaxConnections = await database.query("SHOW MAX_CONNECTIONS")
-  const postgresUsedConnections = await database.query("SELECT numbackends FROM pg_stat_database")
+  const postgresVersion = await database.query("SHOW server_version;");
+  const postgresVersionValue = postgresVersion.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query("SHOW MAX_CONNECTIONS")
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
-    postgres_version: postgresVersion?.rows[0].version,
-    max_connections: postgresMaxConnections?.rows[0].max_connections,
-    used_connections: postgresUsedConnections?.rows[0].numbackends,
+    dependencies: {
+      database: {
+        version: postgresVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    }
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -8,7 +8,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
   
-  expect(responseBody.postgres_version).toContain("PostgreSQL");
-  expect(Number(responseBody.max_connections ?? NaN)).toBeGreaterThan(0);
-  expect(Number(responseBody.used_connections ?? NaN)).toBeGreaterThanOrEqual(0);
+  expect(responseBody.postgres_version).toEqual("16.0");
+  expect(Number(responseBody.max_connections ?? NaN)).toEqual(100);
+  expect(Number(responseBody.used_connections ?? NaN)).toEqual(1);
 });
